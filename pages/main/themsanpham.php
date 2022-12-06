a340950347109422a0672ab43b448a7c0d1c5bb1
ghaa1888 <giangthiha1808@gmail.com>
2022-12-06 12:29:38 +0700

    chuc nang them san pham cua nguoi dung

---
+++ pages/main/themsanpham.php
@@ -0,0 +1,62 @@
+<?php
+    session_start();
+    //thêm sản phẩm vào giỏ hàng
+    include("../../admin/config/config.php");
+    if(isset($_POST['themsanpham'])){
+        $id = $_GET['id_kh'];
+        $songuoi = 1;
+        $sql_number = "Select tenkhoahoc, giakhoahoc, songuoi, anh_kh, id_danhmuc from tblkhoahoc where id_kh = '$id'";
+        $result = mysqli_query($conn,$sql_number);
+        $row = mysqli_fetch_array($result);
+        if($row){
+            $new_product = array(array('id' => $id,'tenkhoahoc' => $row["tenkhoahoc"], 'songuoi' => $songuoi, 'giakhoahoc' => $row["giakhoahoc"], 'hinhanh' => $row['anh_kh']));
+            //kiểm tra giỏ hàng tồn tại hay chưa
+        
+        }
+        header("Location: ../../index.php?quanly=giohang");   
+        // print_r($_SESSION['cart']);
+    }
+    //xóa tất cả sản phẩm
+    if(isset($_GET['xoatatca']) && $_GET['xoatatca']==1){
+        unset($_SESSION['cart']);
+        header("Location: ../../index.php?quanly=giohang");  
+    }
+    //Xóa từng sản phẩm
+   
+    //Cộng và trừ sản phẩm
+    if(isset($_GET['cong'])){
+        $id = $_GET['cong'];
+        foreach($_SESSION['cart'] as $cart_item){
+            if($cart_item['id'] !== $id){
+                $product[] = array('id' => $cart_item['id'],'tenkhoahoc' => $cart_item["tenkhoahoc"], 'songuoi' => $cart_item['songuoi'], 'giakhoahoc' => $cart_item["giakhoahoc"]);
+                $_SESSION['cart'] = $product;
+            }else{
+                $tangsoluong = $cart_item['songuoi']+1;
+                if($cart_item['songuoi'] <= 30){
+                    $product[] = array('id' => $cart_item['id'],'tenkhoahoc' => $cart_item["tenkhoahoc"], 'songuoi' => $tangsoluong, 'giakhoahoc' => $cart_item["giakhoahoc"]);
+                }else{
+                    $product[] = array('id' => $cart_item['id'],'tenkhoahoc' => $cart_item["tenkhoahoc"], 'songuoi' => $cart_item['songuoi'], 'giakhoahoc' => $cart_item["giakhoahoc"]);
+                }
+                $_SESSION['cart'] = $product;
+            }
+        }
+        header("Location: ../../index.php?quanly=giohang"); 
+    }else if($_GET['tru']){
+        $id = $_GET['tru'];
+        foreach($_SESSION['cart'] as $cart_item){
+            if($cart_item['id'] !== $id){
+                $product[] = array('id' => $cart_item['id'],'tenkhoahoc' => $cart_item["tenkhoahoc"], 'songuoi' => $cart_item['songuoi'], 'giakhoahoc' => $cart_item["giakhoahoc"]);
+                $_SESSION['cart'] = $product;
+            }else{
+                $tangsoluong = $cart_item['songuoi']-1;
+                if($cart_item['songuoi'] > 1 ){
+                    $product[] = array('id' => $cart_item['id'],'tenkhoahoc' => $cart_item["tenkhoahoc"], 'songuoi' => $tangsoluong, 'giakhoahoc' => $cart_item["giakhoahoc"]);
+                }else{
+                    $product[] = array('id' => $cart_item['id'],'tenkhoahoc' => $cart_item["tenkhoahoc"], 'songuoi' => $cart_item['songuoi'], 'giakhoahoc' => $cart_item["giakhoahoc"]);
+                }
+                $_SESSION['cart'] = $product;
+            }
+        }
+        header("Location: ../../index.php?quanly=giohang"); 
+    }
+?>
