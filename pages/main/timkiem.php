d4578a9a16030d1675add87ae1a74ab05d5b4414
linh1012 <suga931012@gmail.com>
2022-12-09 19:09:41 +0700

    them chuc nang tim kiem khoa hoc

---
+++ pages/main/timkiem.php
@@ -0,0 +1,22 @@
+<?php
+    $tukhoa = $_REQUEST['keyword'];
+    $timkiem = $_REQUEST['timkiem'];
+    $select_find = "Select * from tblkhoahoc where tenkhoahoc = '$tukhoa'";
+    $result_find = mysqli_query($conn,$select_find);
+?>
+<h3>Search Keywords: <?php echo $tukhoa;?></h3>
+                  <?php
+                      while($row_find = mysqli_fetch_array($result_find)){
+                  ?>
+                    <div class ="col-12 col-sm-6 col-md-4">
+                        <img src="./admin/modules/quanlysanpham/img/<?php echo $row_find["anh_kh"];?>" alt="" class="img-course">
+                        <p class="name-course" >Name Course: <?php echo $row_find["tenkhoahoc"];?></p>
+                        <p class="price-course"><?php echo number_format($row_find["giakhoahoc"],0,',','.'); ?>đ /3 months</p>
+                        <p class="join-course">About: <?php echo $row_find["songuoi"];?> persons</p>
+                        <div class="btn-choose">
+                            <a style="text-decoration:none;" href="index.php?quanly=sanpham&id=<?php echo $row_find["id_kh"];?>" class="btn-course">Choose Course</a>
+                        </div>
+                    </div>
+                  <?php
+                    }
+                  ?>
